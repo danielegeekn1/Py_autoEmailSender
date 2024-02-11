84c6c9f2dc68d8fb5f0a130bbf6cbfa9e7545de1
Daniele Basile <daniele.basile.job@gmail.com>
2024-02-11 12:04:38 +0100

    initial setup

---
+++ text_email.py
@@ -0,0 +1,6 @@
+import getpass 
+import smtplib
+HOST = 'smtp-mail.outlook.com' #server domain name of outlook
+PORT = 587 #server port for smtplib
+FROM_EMAIL = 'daniele.basile.jobol@outlook.com'
+
